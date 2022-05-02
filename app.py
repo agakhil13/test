b532ffe981275b47740ba901586eabbec3f97872
d0t077o <Devang.Tomar@walmart.com>
2022-05-02 21:14:21 +0530

    First Commit!

---
+++ app.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+df = pd.read_csv('./csv/data.csv')
+df_1 = pd.read_csv('./csv/data1.csv')
+
+
+for index, row in df.iterrows():
+    
+    location = df_1.loc[df_1.index[df_1[' Repo Name'] == row[" Repo Name"]]]    # location is a dataframe having the whole matched row inside it!
+
+    if location.empty: 
+
+        print('Do nothing!')
+
+    else:
+
+        print('Caught the match!')
+        # print('Match found at : ' + location)
+        print('Match found at : ' + location[' Last Commit'][1])
