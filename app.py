ca03703a224552f9b16e7337ac6c8ceacfc54d84
d0t077o <Devang.Tomar@walmart.com>
2022-05-02 22:55:53 +0530

    Adding the final changes!

--- app.py
+++ app.py
@@ -5,18 +5,31 @@ df_1 = pd.read_csv('./csv/data1.csv')
 
 
 for index, row in df.iterrows():
-    
-    location = df_1.loc[df_1.index[df_1[' Repo Name'] == row[" Repo Name"]]]    # location is a dataframe having the whole matched row inside it!
 
-    if location.empty: 
+    # location is a dataframe having the whole matched row inside it!
+    location_match_for_repo_name = df_1.loc[df_1.index[df_1[' Repo Name']
+                                                       == row[" Repo Name"]]]
+    location_match_for_last_commit = df_1.loc[df_1.index[df_1[' Last Commit']
+                                                         == row[" Last Commit"]]]
 
-        print('Do nothing!')
+    if location_match_for_repo_name.empty:
+
+        print('Execute some APIs!')
+        df_1.loc[df_1.index[df_1[' Repo Name'] == row[" Repo Name"]],
+                 '  Collaborators'] = 'Replace this with desired string value!'
+        df_1.to_csv('./csv/some-new-file.csv')
 
     else:
 
-        print('Caught the match!')
-        print('Match found at : ' + location)
-        print('Match found at : ' + location[' Last Commit'])
-        
+        print('Now checking if Last commit is same or not in both CSVs!')
 
-        # Now compare location[' Last Commit'] and row[' Last Commit'] and proceed further!
+        if location_match_for_last_commit.empty:  # if last commit is not same in both CSVs
+
+            print('Execute some APIs!')
+            df_1.loc[df_1.index[df_1[' Repo Name'] == row[" Repo Name"]],
+                     '  Collaborators'] = 'Replace this with desired string value!'
+            df_1.to_csv('./csv/some-new-file.csv')
+
+        else:                                     # if last commit is same in both CSVs
+
+            print('Ignoring the repo!')
